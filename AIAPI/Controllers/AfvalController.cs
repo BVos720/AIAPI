3d223c3860f89e3767d720fd64fb8bf9a319334d
jarno <jarnoroubos23@gmail.com>
2026-05-29 11:59:03 +0200

    klasse en basic GET POST

---
+++ AIAPI/Controllers/AfvalController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace P4LU1API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AfvalController : ControllerBase
+{
+
+    [HttpGet(Name = "GetAfval")]
+    public IEnumerable<Afval> Get()
+    {
+
+    }
+
+    [HttpGet(Name = "PostAfval")]
+    public IEnumerable<Afval> Post()
+    {
+        return new Afval[0];
+    }
+}
